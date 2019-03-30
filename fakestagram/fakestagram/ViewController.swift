7eebe499b7b5322293f6e308007e4e9f1bf8cbc8
AntonioArellano <antonio.941027@gmail.com>
2019-03-30 10:43:44 -0600

    First commit

---
+++ fakestagram/fakestagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fakestagram
+//
+//  Created by José Antonio Arellano Mendoza on 3/30/19.
+//  Copyright © 2019 José Antonio Arellano Mendoza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
